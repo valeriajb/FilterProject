02a26244efee1e1b2ea61a3f0c973ed1c4263934
Valeria Jiménez <vjimenezbedoya@gmail.com>
2022-12-09 12:25:00 -0500

    ProjectFilter OK

--- index.js
+++ index.js
@@ -46,24 +46,24 @@ const data = [
 const inputSearch = document.querySelector(".input-search");
 const menuProducts = document.querySelector(".list-products");
 const containerProducts = document.querySelector(".cards-watch");
+const inputRange=document.querySelector(".input-range")
+const numberRange=document.querySelector(".number-range")
 
-const getDataProducts = (products) => {
- 
-  containerProducts.innerHTML = products.map(
+const getDataProducts = (filteredProducts) => {
+  containerProducts.innerHTML = filteredProducts.map(
     (e) =>
       `<div class="card-product">
         <img src=${e.img} alt=${e.name} class="image-product">
         <p class="description-product">${e.name}</p>
-        <span class="price-product">${e.price}</span>
+        <span class="price-product">$${e.price}</span>
       </div>`
   );
 };
 getDataProducts(data);
 
 inputSearch.addEventListener("keyup", (event) => {
-  const value = event.target.value;
+  const value = event.target.value.toLowerCase();
   console.log(value);
-
   if (value) {
     getDataProducts(
       data.filter((item) => item.name.toLowerCase().indexOf(value) !== -1)
@@ -73,13 +73,38 @@ inputSearch.addEventListener("keyup", (event) => {
   }
 });
 
+const getItemsProducts = (products) => {
+  const categoriesData = products.map((item) => item.cat);
+  const categoriesFilter = categoriesData.filter(
+    (item, index) => categoriesData.indexOf(item) === index // Recordemos que el indexOf, retorna la posiciòn de lo que se entre por paràmetro, solo envìa la posiciòn de un elemento, y envia el primer.
+  );
+  const allCategories = ["All",...categoriesFilter]; //Si solo se le pone coma, se toma como si solo ese array fuera un elemento, mientras colcando los 3 puntos me concatena el array, conviertiendo en varios elementos
 
-const getItemsProducts=(products)=>{
-  const dataItems=products.map((e)=>e.cat)
+  menuProducts.innerHTML = allCategories
+    .map((item) => `<li class="item-product">${item}</li>`).join(""); //El join en cada elemnto se inserta lo que se entra por paràmetro en la funciòn
+    console.log(allCategories);
+};
+getItemsProducts(data);
 
-  menuProducts.innerHTML = products
-  .map((e) => `<li class="item-product">${e.cat}</li>`)
-  .join("");
+menuProducts.addEventListener("click", (e) => {
+  const valueCategorie = e.target.textContent;
+  valueCategorie !== "All"
+    ? getDataProducts(data.filter((item) => item.cat === valueCategorie))
+    : getDataProducts(data);
+});
 
-}
-getItemsProducts(data)
+
+
+const priceProducts=data.map((item)=>item.price)
+const priceMin=Math.min(...priceProducts)
+const priceMax=Math.max(...priceProducts)
+
+inputRange.min=priceMin
+inputRange.max=priceMax
+
+inputRange.addEventListener("input",(e)=>{ 
+  const valueRange=e.target.value
+  numberRange.textContent=`$ ${valueRange}`
+
+  getDataProducts(data.filter((item)=>item.price<=valueRange))
+})
